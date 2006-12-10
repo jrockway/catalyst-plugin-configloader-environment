15ea4feb0bba89d9b562462493d3d25cc0392b7b
jon <jon@01d9642f-1d18-0410-bea3-844bd6f27a17>
2006-12-10 07:29:20 +0000

    r8115@foo:  jon | 2006-12-09 20:14:20 -0600  CPCE: inital add
    git-svn-id: svn://svn.jrock.us/cpan_modules/Catalyst-Plugin-ConfigLoader-Environment@143 01d9642f-1d18-0410-bea3-844bd6f27a17

---
+++ lib/Catalyst/Plugin/ConfigLoader/Environment.pm
@@ -0,0 +1,101 @@
+package Catalyst::Plugin::ConfigLoader::Environment;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Catalyst::Plugin::ConfigLoader::Environment - The great new Catalyst::Plugin::ConfigLoader::Environment!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Catalyst::Plugin::ConfigLoader::Environment;
+
+    my $foo = Catalyst::Plugin::ConfigLoader::Environment->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jonathan Rockway, C<< <jrockway at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-catalyst-plugin-configloader-environment at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Catalyst-Plugin-ConfigLoader-Environment>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Catalyst::Plugin::ConfigLoader::Environment
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Catalyst-Plugin-ConfigLoader-Environment>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Catalyst-Plugin-ConfigLoader-Environment>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Catalyst-Plugin-ConfigLoader-Environment>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Catalyst-Plugin-ConfigLoader-Environment>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006 Jonathan Rockway, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Catalyst::Plugin::ConfigLoader::Environment
